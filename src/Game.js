08e75ba654b40712836729686cd9da90269e29c4
Shelby Gluck <shelbygluck@gmail.com>
2020-03-08 12:49:43 -0400

    game class defined

--- src/Game.js
+++ src/Game.js
@@ -6,6 +6,39 @@ const WIDTH = 800
 const HEIGHT = 600
 
 class Game extends React.Component {
+    constructor() {
+        super()
+        this.rows = HEIGHT/CELL_SIZE
+        this.columns = WIDTH/CELL_SIZE
+        this.board = this.makeEmptyBoard()
+
+        state = {
+            cells: []
+        }
+    }
+
+    makeEmptyBoard() {
+        let board = []
+        for (let y = 0; y < this.rows; y++) {
+            board[y] = []
+            for (let x = 0; x < this.columns; x++) {
+                board[y][x] = false
+            }
+        }
+        return board
+    }
+
+    makeCells() {
+        let cells = []
+        for (let y = 0; y < this.rows; y++) {
+            for (let x = 0; x < this.columns; x++) {
+                if (this.board[y][x]) {
+                    cells.push({x,y})
+                }
+            }
+        }
+    }
+
     render() {
         return (
             <div>

46c0b5c39d9611da42859a18013bc352ae9dd414
Shelby Gluck <shelbygluck@gmail.com>
2020-03-08 14:09:57 -0400

    Cell class added but map bug

--- src/Game.js
+++ src/Game.js
@@ -11,10 +11,12 @@ class Game extends React.Component {
         this.rows = HEIGHT/CELL_SIZE
         this.columns = WIDTH/CELL_SIZE
         this.board = this.makeEmptyBoard()
-
-        state = {
+        
+        this.state = {
             cells: []
         }
+
+        this.handleClick = this.handleClick.bind(this)
     }
 
     makeEmptyBoard() {
@@ -39,13 +41,63 @@ class Game extends React.Component {
         }
     }
 
+    handleClick(event) {
+        //retrieve click position, convert to relative position, calculate rows/columns of cell being clicked
+        const elemOffset = this.getElementOffset()
+        const offsetX = event.clientX - elemOffset.x
+        const offsetY = event.clientY - elemOffset.y
+        const x = Math.floor(offsetX / CELL_SIZE)
+        const y = Math.floor(offsetY / CELL_SIZE)
+
+        //if the cell is within grid limits, reverse its boolean status
+        if (x >= 0 && x <= this.columns && y >= 0 && y <= this.rows) {
+            this.board[y][x] = !this.board[y][x]
+        }
+
+        this.setState({
+            cells: this.makeCells()
+        })
+    }
+
+    getElementOffset() {
+        //calculate position of board element
+        const rect = this.boardRef.getBoundingClientRect()
+        //getBCR() returns size of element and its position relative to viewport
+        const doc = document.documentElement
+
+        return {
+            x: (rect.left + window.pageXOffset) - doc.clientLeft,
+            //element.clientLeft return width of left border of element in pixels
+            y: (rect.top + window.pageYOffset) - doc.clientTop
+        }
+    }
+
     render() {
+        const { cells } = this.state
         return (
             <div>
                 <div className="Board" style={{ width: WIDTH, height: HEIGHT,
-                backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px` }}>
+                backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`}}
+                onClick={this.handleClick} ref={(n) => { this.boardRef = n}}>
+                    {cells.map(cell => {
+                        return <Cell x={cell.x} y={cell.y} key={`${cell.x},${cell.y}`}/>
+                    })}
                 </div>
             </div>
+        )
+    }
+}
+
+class Cell extends React.Component {
+    render() {
+        const { x, y } = this.props
+        return (
+            <div className="Cell" style={{
+                left: `${CELL_SIZE * x + 1}px`,
+                top: `${CELL_SIZE * y + 1}px`,
+                width: `${CELL_SIZE - 1}px`,
+                height: `${CELL_SIZE - 1}px`
+            }} />
         )
     }
 }
